0599f547ed51bc05aacac48ecfe1eaa049b4f96d
зизя <zizya@zizas-MacBook-Air.local>
2026-04-22 01:16:43 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ apt-get update
 # Posted by EakzIT, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-04-22, License - CC BY-SA 4.0
 
-sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev libfreetype6-dev
+apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev libfreetype6-dev
 
 echo "INSTALLED"
 
