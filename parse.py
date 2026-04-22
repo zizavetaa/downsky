2da820da7b8a8b62e83113c34ba0a28e1634f434
зизя <zizya@zizas-MacBook-Air.local>
2026-04-22 15:19:44 +0300

    change chrome options

--- parse.py
+++ parse.py
@@ -18,6 +18,8 @@ options = Options()
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--remote-debugging-port=9222")
 
 
 class Parser:
